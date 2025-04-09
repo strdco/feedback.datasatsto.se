07c2873c219d4ccea6cba8d5aa2040bd673211a0
Daniel Hutmacher <daniel@strd.co>
2025-04-09 15:48:56 +0200

    Azure Web App health check

--- server.js
+++ server.js
@@ -108,6 +108,18 @@ app.listen(serverPort, () => console.log('READY.'));
 
 
 /*-----------------------------------------------------------------------------
+  Azure Linux App Service Plan health check request:
+  ---------------------------------------------------------------------------*/
+
+app.get('/robots933456.txt', function (req, res, next) {
+    console.log("Azure health check: OK.");
+    res.status(200).send("OK");
+});
+
+
+
+
+/*-----------------------------------------------------------------------------
   Serve up the blank template page. "the-business.js" will perform the
   appropriate action on the browser side, depending on the URL.
   ---------------------------------------------------------------------------*/
